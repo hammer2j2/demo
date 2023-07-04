03e0c8c9fbf420240be25df3961f2f1655c3633c
Clayton Han-Mitchell <hammer2j2@gmail.com>
2023-07-04 15:02:37 -0700

    wip

--- src/main/java/com/example/demo/statsController.java
+++ src/main/java/com/example/demo/statsController.java
@@ -2,11 +2,21 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.service.StatsService;
 
 @RestController
-public class statsController {
+public class StatsController {
+    @Autowired
+    private final StatsService statsService; 
+
+    public StatsController(StatsService statsService){
+        this.statsService = statsService;
+    }
+
     @GetMapping("/stats")
     public String index() {
+        
         return "This will be a list from S3 or DynamoDB";
     }
 }
